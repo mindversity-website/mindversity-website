34d69c1ec3cf2007fa59e87c3f8f04b5ab9d0c5c
mindversity-website <76635031+mindversity-website@users.noreply.github.com>
2020-12-27 01:35:01 -0600

    Changed The Shrink Space logo and added links

--- components/Partners/index.tsx
+++ components/Partners/index.tsx
@@ -9,24 +9,30 @@ const Partners: React.FC = () => {
                 <h2>Partners</h2>
             </div>
             <br></br>
-            <div className={styles.partner}>
-                <img
-                    src="https://www.stevefund.org/wp-content/uploads/2019/03/The-Steve-Fund-logo-vertical-color.png"
-                    alt="Partner logo"
-                ></img>
-            </div>
-            <div className={styles.partner}>
-                <img
-                    src="https://www.artwithimpact.org/wp-content/uploads/awi_logo_020711_0-1.png"
-                    alt="Partner logo"
-                ></img>
-            </div>
-            <div className={styles.partner}>
-                <img
-                    src="https://pbs.twimg.com/profile_images/1102970449472950273/xGIOGqeH_400x400.jpg"
-                    alt="Partner logo"
-                ></img>
-            </div>
+            <a href="https://www.stevefund.org/">
+                <div className={styles.partner}>
+                    <img
+                        src="https://www.stevefund.org/wp-content/uploads/2019/03/The-Steve-Fund-logo-vertical-color.png"
+                        alt="Partner logo"
+                    ></img>
+                </div>
+            </a>
+            <a href="https://www.artwithimpact.org/">
+                <div className={styles.partner}>
+                    <img
+                        src="https://www.artwithimpact.org/wp-content/uploads/awi_logo_020711_0-1.png"
+                        alt="Partner logo"
+                    ></img>
+                </div>
+            </a>
+            <a href="https://theshrinkspace.com/">
+                <div className={styles.partner}>
+                    <img
+                        src="https://theshrinkspace.com/packs/application/images/shrinkspace-logo-white-e79af571ed155beab85f4941eaa1045a.svg"
+                        alt="Partner logo"
+                    ></img>
+                </div>
+            </a>
         </section>
     );
 };
